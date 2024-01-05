5fa379a8592240f480570f3b199a961d13b152d3
Scott Kennedy <skennedy-bm@live.com>
2024-01-05 07:59:15 -0400

    Updated exercise 7 plotly, encountering indent errors need to fix white space

--- 07_lab_Plotly.py
+++ 07_lab_Plotly.py
@@ -1,46 +1,72 @@
+# Run in Interactive Window for VSCode, can save output as j#Jupyter notebook
 # Import required libraries
 import pandas as pd
-import plotly.graph_objects as go
+import plotly.graph_objects as px
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html
+from dash import dcc
 from dash.dependencies import Input, Output
 
 # Read the airline data into pandas dataframe
-airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', 
-                            encoding = "ISO-8859-1",
-                            dtype={'Div1Airport': str, 'Div1TailNum': str, 
-                                   'Div2Airport': str, 'Div2TailNum': str})
+spacex_df =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/spacex_launch_dash.csv')
+
 # Create a dash application
 app = dash.Dash(__name__)
-                               
-app.layout = html.Div(children=[ html.H1('Airline Performance Dashboard', 
-                                style={'textAlign': 'center', 'color': '#503D36',
-                                'font-size': 40}),
-                                html.Div(["Input Year: ", dcc.Input(id='input-year', value='2010', 
-                                type='number', style={'height':'50px', 'font-size': 35}),], 
-                                style={'font-size': 40}),
-                                html.Br(),
-                                html.Br(),
-                                html.Div(dcc.Graph(id='line-plot')),
-                                ])
 
-# add callback decorator
-@app.callback( Output(component_id='line-plot', component_property='figure'),
-               Input(component_id='input-year', component_property='value'))
+app.layout = html.Div([dcc.Dropdown(id='site-dropdown',
+                options=[
+                    {'label': 'All Sites', 'value': 'ALL'},
+                    {'label': 'CCAFS LC-40', 'value': 'CCAFS LC-40'},
+                    {'label': 'CCAFS SLC-40', 'value': 'CCAFS SLC-40'},
+                    {'label': 'KSC LC-39A', 'value': 'KSC LC-39A'},
+                    {'label': 'VAFB SLC-4E', 'value': 'VAFB SLC-4E'}
+                ],
+                value='ALL',
+                placeholder="Launch Site selection",
+                searchable='True'),
+                html.Div(id='dd-output-container')
+                ,
+                dcc.RangeSlider(id='payload-slider',
+                                    min=0, max=10000, step=1000,
+                                    marks={0: '0',100: '100'},
+                                    value=[min, max]),
+                html.Div(id='dd-output-container')
+                ]
+                )
 
-# Add computation to callback function and return graph
-def get_graph(entered_year):
-    # Select 2019 data
-    df =  airline_data[airline_data['Year']==int(entered_year)]
-    
-    # Group the data by Month and compute average over arrival delay time.
-    line_data = df.groupby('Month')['ArrDelay'].mean().reset_index()
+# Function decorator to specify function input and output
+@app.callback(Output(component_id='success-pie-chart', component_property='figure'),
+              Input(component_id='site-dropdown', component_property='value'))
 
-    fig = go.Figure(data=go.Scatter(x=line_data['Month'], y=line_data['ArrDelay'], mode='lines', marker=dict(color='green')))
-    fig.update_layout(title='Month vs Average Flight Delay Time', xaxis_title='Month', yaxis_title='ArrDelay')
-    return fig
+def get_pie_chart(entered_site):
+    filtered_df = spacex_df
+    if entered_site == 'ALL':
+        fig = px.pie(data=filtered_df, values='class', 
+        names='pie chart names', 
+        title='title')
+        return fig
+    else:
+        # return the outcomes pie-chart for a selected site
+
+@app.callback(Output(component_id='success-payload-scatter-chart', component_property='figure'),
+              [Input(component_id='site-dropdown', component_property='value'), Input(component_id="payload-slider", component_property="value")])
+
+def get_scatter_chart(entered_site):
+    filtered_df = spacex_df
+    if entered_site == 'ALL':
+        fig = px.scatter(data=filtered_df, x='Payload Mass (kg)', y='class', 
+        names='scatter chart names', 
+        color = 'Booster Version Category',
+        title='title')
+        return fig
+    else:
+        fig = px.scatter(data=filtered_df[entered_site], x='Payload Mass (kg)', y='class', 
+        names='scatter chart names', 
+        color = 'Booster Version Category',
+        title='title')
+        return fig
+        # return the outcomes piechart for a selected site
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
